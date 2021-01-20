03340ca5dedbd51705eca5495e136859ae842a78
Talha <talhaanwar.anwar@gmail.com>
2021-01-21 00:20:35 +0530

    Set up tailwind css for the project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-const Index = () => (
-  <p>
-Hello, Clipboard health!
-  </p>
-)
-export default Index
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen bg-gray-200">
+      <Head>
+        <title>Clipboard Health</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./styles/**/*.css'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
