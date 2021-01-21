19b2cb64d27460423aaebdbadf2c17ca558109ac
Talha <talhaanwar.anwar@gmail.com>
2021-01-22 01:25:41 +0530

    Shifted show more filter to its own Modal component

---
+++ components/jobs/filterModal.js
@@ -0,0 +1,45 @@
+export const FilterModal = ({data,changeFilterInfo,setShowModal}) => {
+  return (
+      <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={() => setShowModal(false)}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Department
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      X
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-3 flex-auto">
+                  <div className="grid grid-cols-4 gap-2">
+                    {data.map((el, index) =>(
+                        <div key={index} onClick={(e) => changeFilterInfo(true, el['key'])}>
+                        <span className="ml-2 text-gray-700">{el['key']}</span>
+                <span className="text-xs text-gray-300">{el['doc_count']}</span>
+                        </div>
+                        ))}
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      
+  );
+};
+
+export default FilterModal;
