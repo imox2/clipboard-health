8dc7b5cf8df47081791d795de4b4ebc7e0c48c2a
Talha <talhaanwar.anwar@gmail.com>
2021-01-22 00:15:03 +0530

    Added show more modal + bound modal to filter and data change

--- components/jobs/index.js
+++ components/jobs/index.js
@@ -8,17 +8,28 @@ export default function Jobs({ searchText }) {
     const [jobs, setJobs] = useState();
     const [filters, setFilters] = useState();
     const [sortMenus, setSortMenus] = useState({
-      'Location': false,
-      'Role': false,
-      'Department': false,
-      'Education': false,
-      'Experience': false,
+      'Location': '',
+      'Role': '',
+      // 'Department': '',
+      // 'Education': '',
+      'Experience': '',
     });
+
+    const selectSort = (key) => {
+      console.log("selectSort:",key);
+    const sortDataCopy = { ...sortMenus };
+    const value = sortDataCopy[key];
+    if (value === "asc") sortDataCopy[key] = "desc";
+    if (value === "desc") sortDataCopy[key] = '';
+    if (value === '') sortDataCopy[key] = "asc";
+    setSortMenus(sortDataCopy);
+    console.log("sortDataCopy:",sortDataCopy);
+  };
 
     const selectFilters = (checked, filter) => {
       const filtersCopy = !filters ? [] : JSON.parse(JSON.stringify(filters));
       console.log("filtersCopy:",filtersCopy);
-      if(checked) {
+      if(checked && filtersCopy.indexOf(filter) == -1) {
         filtersCopy.push(filter);
       } else {
         const index = filtersCopy.indexOf(filter);
@@ -27,7 +38,6 @@ export default function Jobs({ searchText }) {
         }
       }
       setFilters(filtersCopy);
-      console.log("filtersCopy:",filtersCopy);
     };
 
     const getJobsData = async () => {
@@ -36,6 +46,12 @@ export default function Jobs({ searchText }) {
       if(filters && filters.length > 0) {
         url = url + `&filters=${filters.toString()}`
       }
+       const sortDataCopy = { ...sortMenus };
+       Object.keys(sortDataCopy).forEach(key => {
+        if(sortDataCopy[key]) {
+          url = url + `&${key}=${sortDataCopy[key]}`
+        }
+       });
       const result = await axios(
          url
       );
@@ -50,7 +66,7 @@ export default function Jobs({ searchText }) {
     useEffect(() => {
       setJobs(null);
       getJobsData();
-  }, [searchText,filters]);
+  }, [searchText,filters,sortMenus.Location, sortMenus.Role, sortMenus.Experience]);
 
     return (
         <div className="m-4 flex flex-row lg:space-x-4">
@@ -60,18 +76,19 @@ export default function Jobs({ searchText }) {
             <div className="w-full bg-white  lg:border lg:border-gray-200">
             <div className="grid grid-cols-2">
   <div className="space-x-3 p-3.5 items-center">{jobs ? computeTotalJobs(jobs) + ' Job posting' : ''}</div>
-  <div className="flex flex-row space-x-3 p-3.5 items-center">
+  <div className="flex flex-row space-x-6 p-3.5 items-center">
   <div>Sort by</div>
   {sortMenus ? (
                 Object.keys(sortMenus).map((sortMenu, index) => (
-                    <div key={index}>
+                    <div key={index} 
+                    onMouseDown={() => selectSort(sortMenu)}>
                     {sortMenu}
                     <span className="inline-block">
-                    {sortMenus[sortMenu] ?
-                      <svg viewBox="0 0 24 24" fill="black" width="16px" height="16px">
+                    {sortMenus[sortMenu] === "desc" && <svg viewBox="0 0 24 24" fill="black" width="16px" height="16px">
     <rect fill="none" height="24" width="24" />
     <path d="M19,15l-1.41-1.41L13,18.17V2H11v16.17l-4.59-4.59L5,15l7,7L19,15z" />
-  </svg>:<svg viewBox="0 0 24 24" fill="black" width="16px" height="16px">
+  </svg>}
+                    {sortMenus[sortMenu] === "asc" && <svg viewBox="0 0 24 24" fill="black" width="16px" height="16px">
     <rect fill="none" height="24" width="24" />
     <path d="M5,9l1.41,1.41L11,5.83V22H13V5.83l4.59,4.59L19,9l-7-7L5,9z" />
   </svg>}
